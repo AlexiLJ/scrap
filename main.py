eb585d173f890c2ecc730f294fedea9705396e31
Alex Leiko <leiko.oleksandr@gmail.com>
2021-08-27 18:38:31 +0200

    filering shit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open('home.html', 'r') as htm_file:
-    content = htm_file.read()
+
+html_text = requests.get('https://www.jobs.cz/prace/praha/?q%5B%5D=python').text
+# print(html_text)
+# with open('home.html', 'r') as htm_file:
+#     content = htm_file.read()
     
-    soup = BeautifulSoup(content, 'lxml')
+soup = BeautifulSoup(html_text, 'lxml')
+# jobs = soup.find_all('div', class_="grid__item")
+company_name=soup.find('h3', class_='search-list__main-info__title')
+job=company_name.find('a').text
+print(job)
   
-    course_cards = soup.find_all('div', class_="card")
-    for c in course_cards:
-        print(f'{c.h5.text} costs {c.a.text.split()[-1]}')
+#     course_cards = soup.find_all('div', class_="card")
+#     for c in course_cards:
+#         print(f'{c.h5.text} costs {c.a.text.split()[-1]}')
 
