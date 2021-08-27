8c206c58d9f87ea679a0787f169217cedc93650d
Alex <leiko.oleksandr@gmail.com>
2021-08-27 12:34:34 +0200

    start

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as htm_file:
     content = htm_file.read()
-    # print(content)
+    
+    soup = BeautifulSoup(content, 'lxml')
+
+    # print(soup.prettify())  
