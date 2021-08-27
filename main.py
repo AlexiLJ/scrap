9c3c0c1967d3d3368c74df06dd2ecb13cbf8ebc2
Alex <leiko.oleksandr@gmail.com>
2021-08-27 15:46:49 +0200

    stuff

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ with open('home.html', 'r') as htm_file:
     content = htm_file.read()
     
     soup = BeautifulSoup(content, 'lxml')
+  
+    course_cards = soup.find_all('div', class_="card")
+    for c in course_cards:
+        print(f'{c.h5.text} costs {c.a.text.split()[-1]}')
 
-    # print(soup.prettify())  
