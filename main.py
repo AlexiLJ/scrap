9b9bd0b3505562a3f37c01db7dd1c7a09be6afb6
Alex Leiko <leiko.oleksandr@gmail.com>
2021-09-19 20:22:57 +0200

    refactoring Scrapper object

--- main.py
+++ main.py
@@ -5,50 +5,59 @@ import json
 import pandas as pd
 
 
-def scrape_product_detail_page(product_detail_url):
-    yield_=dict()
-    product_page = requests.get(product_detail_url)
-    soup = BeautifulSoup(product_page.content, 'lxml')
-    yield_["model"] = soup.find(id="detail_page").h1.text
-    yield_["url"] = product_detail_url
-    yield_["main_photo_path"] = soup.find(id="nahled")["src"]
+class Scrapper():
+    def __init__(self, product_detail_url) -> None:
 
-    links = [link.img['src'] for link in soup.find_all(class_="html5lightbox")]
-    if len(links) == 0: yield_['additional_photo_paths']=None
-    else: yield_['additional_photo_paths']=links
+        self.yield_ = dict()
+        self.product_detail_url = product_detail_url
+        self.product_page = requests.get(product_detail_url)
+        self.soup = BeautifulSoup(self.product_page.content, 'lxml')
+    
+    
+    def table_extractor(self, df_table, spec_col='spec', data_col='data', **kwargs):
 
-    price  = soup.find(class_="cena").span.text.split()[0].split('.') 
-    yield_["price"] = int(''.join(price))
-    yield_["model_year"] = int(''.join(price))
+        table_dict=dict()
+        for key, value in kwargs.items():            
+            table_dict[key]= df_table.loc[df_table[(df_table[spec_col] == value)].index[0]][data_col]
+        return table_dict
 
-    #Here I manage specs in the html table through pandas
+
+    def product_detail(self):
+        
+        self.yield_["model"] = self.soup.find(id="detail_page").h1.text
+        self.yield_["url"] = self.product_detail_url
+        self.yield_["main_photo_path"] = self.soup.find(id="nahled")["src"]
+        links = [link.img['src'] for link in self.soup.find_all(class_="html5lightbox")]
+        if len(links) == 0: self.yield_['additional_photo_paths']=None
+        else: self.yield_['additional_photo_paths']=links
+        price  = self.soup.find(class_="cena").span.text.split()[0].split('.') 
+        self.yield_["price"] = int(''.join(price))
+        self.yield_["model_year"] = int(''.join(price))
+
+        #Here I manage specs in the html table through pandas
+        tab = self.soup.find_all('table', class_='spec')
+        table_list=[]
+        for t in tab:
+            table_rows = t.find_all('tr')
+            for tr in table_rows:
+                td = tr.find_all('td')
+                row = [tr.text for tr in td]
+                table_list.append(row[1:])
+        table=pd.DataFrame(table_list, columns=['spec', 'data'])       
+        table_data={"weight" : "Hmotnost", "frame": "Rám"}
+        year = table[table['spec'] == "Ročník"]
+        self.yield_['model_year'] = int(year.iloc[0]['data'])        
+        self.yield_["parameters"] = self.table_extractor(table, **table_data)
     
-    tab = soup.find_all('table', class_='spec')
-    table_list=[]
-    for t in tab:
-        table_rows = t.find_all('tr')
-        for tr in table_rows:
-            td = tr.find_all('td')
-            row = [tr.text for tr in td]
-            table_list.append(row[1:])
-    table=pd.DataFrame(table_list, columns=['spec', 'data'])
-    # table = table.where(pd.notnull(table), None)
-    # table=dumps(table.where(pd.notnull(table), None))
-    
-    
-    year = table[table['spec'] == "Ročník"]
-    yield_['model_year'] = int(year.iloc[0]['data'])
-    
-    yield_["parameters"] ={
-        "weight" : table.loc[table[(table['spec'] == "Hmotnost")].index[0]]['data'],
-        "frame": table.loc[table[(table['spec'] == "Rám")].index[0]]['data']
-        }
-   
-    return yield_
+        return self.yield_
+
+    def pandas__df(self):
+        pass
+
 
 def main(name, url_links):
     with open(name, 'a') as f:
-        data=[scrape_product_detail_page(task) for task in url_links] #brute force
+        data=[Scrapper(task).product_detail() for task in url_links] #brute force
         json.dump(data, f, indent=4)
        
 if __name__ == '__main__':
@@ -58,7 +67,6 @@ if __name__ == '__main__':
         "https://www.lapierre-bike.cz/produkt/lapierre-crosshill-50/6037",
         "https://www.lapierre-bike.cz/produkt/lapierre-ezesty-am-ltd-ultimate/5951",
         "https://www.lapierre-bike.cz/produkt/lapierre-ezesty-am-90-ultimate/5950"
-
     ]
 
     main("top-5-bikes.json", tasks)
