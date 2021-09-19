6d87871f80708e564065c2451dd209efd75672fc
Alex Leiko <leiko.oleksandr@gmail.com>
2021-09-19 19:11:20 +0200

    init script

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
+import json
+import pandas as pd
 
 
-html_text = requests.get('https://www.jobs.cz/prace/praha/?q%5B%5D=python').text
+def scrape_product_detail_page(product_detail_url):
+    yield_=dict()
+    product_page = requests.get(product_detail_url)
+    soup = BeautifulSoup(product_page.content, 'lxml')
+    yield_["model"] = soup.find(id="detail_page").h1.text
+    yield_["url"] = product_detail_url
+    yield_["main_photo_path"] = soup.find(id="nahled")["src"]
 
-# print(html_text)
-# with open('home.html', 'r') as htm_file:
-#     content = htm_file.read()
+    links = [link.img['src'] for link in soup.find_all(class_="html5lightbox")]
+    if len(links) == 0: yield_['additional_photo_paths']=None
+    else: yield_['additional_photo_paths']=links
+
+    price  = soup.find(class_="cena").span.text.split()[0].split('.') 
+    yield_["price"] = int(''.join(price))
+    yield_["model_year"] = int(''.join(price))
+
+    #Here I manage specs in the html table through pandas
+    
+    tab = soup.find_all('table', class_='spec')
+    table_list=[]
+    for t in tab:
+        table_rows = t.find_all('tr')
+        for tr in table_rows:
+            td = tr.find_all('td')
+            row = [tr.text for tr in td]
+            table_list.append(row[1:])
+    table=pd.DataFrame(table_list, columns=['spec', 'data'])
+    # table = table.where(pd.notnull(table), None)
+    # table=dumps(table.where(pd.notnull(table), None))
+    
+    
+    year = table[table['spec'] == "Ročník"]
+    yield_['model_year'] = int(year.iloc[0]['data'])
+    
+    yield_["parameters"] ={
+        "weight" : table.loc[table[(table['spec'] == "Hmotnost")].index[0]]['data'],
+        "frame": table.loc[table[(table['spec'] == "Rám")].index[0]]['data']
+        }
+   
+    return yield_
+
+def main(name, url_links):
+    with open(name, 'a') as f:
+        data=[scrape_product_detail_page(task) for task in url_links] #brute force
+        json.dump(data, f, indent=4)
+       
+if __name__ == '__main__':
+    tasks = [
+        "https://www.lapierre-bike.cz/produkt/aircode-drs-80/5934", 
+        "https://www.lapierre-bike.cz/produkt/spicy-cf-79/5993",
+        "https://www.lapierre-bike.cz/produkt/lapierre-crosshill-50/6037",
+        "https://www.lapierre-bike.cz/produkt/lapierre-ezesty-am-ltd-ultimate/5951",
+        "https://www.lapierre-bike.cz/produkt/lapierre-ezesty-am-90-ultimate/5950"
+
+    ]
+
+    main("top-5-bikes.json", tasks)
 
 
-soup = BeautifulSoup(html_text, 'lxml')
+    
 
-# jobs = soup.find_all('div', class_="grid__item")
 
-company_name=soup.find_all('h3', class_='search-list__main-info__title')
-
-for x in company_name:
-    l = x.find('a').text
-    print(l)
-  
-#     course_cards = soup.find_all('div', class_="card")
-#     for c in course_cards:
-#         print(f'{c.h5.text} costs {c.a.text.split()[-1]}')
-
+        
+   
